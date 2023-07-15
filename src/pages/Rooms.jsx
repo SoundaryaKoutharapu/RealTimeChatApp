c3cd5e89247ffbf0bf8dd868353eb6ccf8332bcb
Soundarya Koutharapu <soundaryakoutharapu@gmail.com>
2023-07-15 20:35:43 +0530

    create messages

--- src/pages/Rooms.jsx
+++ src/pages/Rooms.jsx
@@ -1,40 +1,89 @@
 import { useState, useEffect } from "react"
 import { databases, DATABASE_ID, COLLECTION_ID_MESSAGES } from "../appWriteConfig"
-
+import { ID, Query } from "appwrite"
 
 const Rooms = () => {
 
-const [messages, setMessages] = useState([])
-
+  const [messages, setMessages] = useState([])
+  const [messageBody, setMessageBody] = useState('')
 
   useEffect(() => {
     getMessages()
   }, [])
 
+  const handleSubmit = async (e) => {
+    e.preventDefault()
+
+    let payload = {
+      body: messageBody
+    }
+
+    let response = await databases.createDocument(
+      DATABASE_ID,
+      COLLECTION_ID_MESSAGES,
+      ID.unique(),
+      payload
+    )
+
+    console.log('Created!', response);
+
+    setMessages(prevState => [response, ...messages])
+
+    setMessageBody('')
+  }
 
   const getMessages = async () => {
-    const response = await databases.listDocuments(DATABASE_ID, COLLECTION_ID_MESSAGES)
+    const response = await databases.listDocuments(
+      DATABASE_ID,
+       COLLECTION_ID_MESSAGES,
+       [
+        Query.orderDesc("$createdAt"),
+        Query.limit(5)
+       ]
+       )
     console.log('RESPONSE:', response);
     setMessages(response.documents)
   }
 
+  const deleteMessage = async (message_id) =>
+  {
+     databases.deleteDocument(DATABASE_ID, COLLECTION_ID_MESSAGES, document_id)
+  }
 
   return (
     <main className="container">
-    <div className="room--container">
-      <div>
-        {messages.map(message => (
-          <div key={message.$id} className="message--wrapper">
-            <div className="messages--header">
-              <small className="message-timestamp">{message.$createdAt}</small>
-            </div>
-            <div className="message--body">
-              <span>{message.body}</span>
-            </div>
+      <div className="room--container">
+
+        <form onSubmit={handleSubmit} id="message--form">
+          <div>
+            <textarea
+            maxLength='1000'
+              required
+              placeholder="say something.."
+              onChange={(e) => setMessageBody(e.target.value)}
+              value={messageBody}>
+            </textarea>
           </div>
-        ))}
+          <div className="send-btn--wrapper">
+            <input className="btn btn--secondary" type="submit" value='send' />
+          </div>
+        </form>
+
+        <div>
+          {messages.map(message => (
+            <div key={message.$id} className="message--wrapper">
+              <div className="messages--header">
+                <small className="message-timestamp">{message.$createdAt}</small>
+              </div>
+              <div className="message--body">
+                <span>{message.body}</span>
+              </div>
+            </div>
+          ))}
+        </div>
+
+
       </div>
-    </div>
     </main>
   )
 }
